58cc87db0a825cd43b4a2be83d970f9525fca760
rkdvs222 <rkdvs222@gmail.com>
2024-03-07 15:39:22 +0900

    :up: eslint설정, 패키지 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const env = process.env.NODE_ENV;
 const nextConfig = async () => {
@@ -14,6 +16,9 @@ const nextConfig = async () => {
     output: env == "development" ? "standalone" : "export",
     images: {
       unoptimized: true,
+    },
+    sassOptions: {
+      includePaths: [path.join(__dirname, "./src/styles/")],
     },
   };
 
